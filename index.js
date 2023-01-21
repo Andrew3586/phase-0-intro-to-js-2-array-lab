f39a71bf89523c76a2901874131b9c4039c12d8b
andrew <andrew_3586@iprimus.com.au>
2023-01-21 11:12:47 +1100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (charlie) {
+  cats.push(charlie);
+}
+
+
+
+function  destructivelyRemoveLastCat (charlie) {
+
+cats.pop(charlie)
+}
+
+function destructivelyPrependCat(charlie) {
+
+cats.unshift(charlie)
+
+
+}
+
+function destructivelyRemoveFirstCat() {
+
+  cats.shift()
+}
+
+function appendCat (name) {
+
+  return [...cats, name];
+}
+
+function   prependCat(name) {
+
+  return [name, ...cats];
+}
+
+function  removeLastCat(name) {
+
+
+}
+
+function removeFirstCat () {
+  return cats.slice(1);
+}
+
+function removeLastCat () {
+  return cats.slice(0, cats.length - 1);
+}
+
